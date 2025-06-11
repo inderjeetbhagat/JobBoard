d6baf230eee32ac1a16461be796920ca02d960ad
Inderjeet Bhagat <inderjitbhagat3@gmail.com>
2025-06-12 03:36:21 +0530

    Fixed HTTP Response by using respinse Entity

--- src/main/java/com/jobapp/first/job/JobController.java
+++ src/main/java/com/jobapp/first/job/JobController.java
@@ -1,10 +1,9 @@
 package com.jobapp.first.job;
 
 import org.springframework.boot.autoconfigure.batch.BatchProperties;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,13 +18,21 @@ private JobService jobService;
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll(){
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll(){
+        return new ResponseEntity<>(jobService.findAll(), HttpStatus.OK);
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
+    public ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
-        return "Job Added Successfully";
+        return new ResponseEntity<>("Job Added Successfully", HttpStatus.OK);
+    }
+
+    @GetMapping("/jobs/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id){
+        Job job = jobService.getJobById(id);
+        if(job != null)
+          return new ResponseEntity<>(job, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND) ;
     }
 }
