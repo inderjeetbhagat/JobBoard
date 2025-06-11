31238c7d935abe5251a9f72a3303ee1994d1affc
Inderjeet Bhagat <inderjitbhagat3@gmail.com>
2025-06-12 03:01:00 +0530

    Automated id updation

---
+++ src/main/java/com/jobapp/first/job/impl/JobServiceImpl.java
@@ -0,0 +1,25 @@
+package com.jobapp.first.job.impl;
+
+import com.jobapp.first.job.Job;
+import com.jobapp.first.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService {
+    private List<Job> jobs = new ArrayList<>();
+    private Long nextId = 1L;
+
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        job.setId(nextId++);
+        jobs.add(job);
+    }
+}
