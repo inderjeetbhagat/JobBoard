ea036206b79c1bcb93cb8c8cdbaef9572ed21c61
Inderjeet Bhagat <inderjitbhagat3@gmail.com>
2025-06-21 20:13:34 +0530

    Added H2 Database

--- src/main/java/com/jobapp/first/job/impl/JobServiceImpl.java
+++ src/main/java/com/jobapp/first/job/impl/JobServiceImpl.java
@@ -1,66 +1,65 @@
 package com.jobapp.first.job.impl;
 
 import com.jobapp.first.job.Job;
+import com.jobapp.first.job.JobRepository;
 import com.jobapp.first.job.JobService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobServiceImpl implements JobService {
-    private List<Job> jobs = new ArrayList<>();
-    private Long nextId = 1L;
+//    private List<Job> jobs = new ArrayList<>();
+    JobRepository jobRepository;
+
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+
+    }
 
     @Override
     public List<Job> findAll() {
-        return jobs;
+        return jobRepository.findAll();
+
     }
 
     @Override
     public void createJob(Job job) {
-        job.setId(nextId++);
-        jobs.add(job);
+        jobRepository.save(job);
     }
 
     @Override
     public Job getJobById(Long id) {
-        for (Job job : jobs) {
-            if (job.getId().equals(id)) {
-                return job;
-            }
-
-        }
-        return null;
+        return jobRepository.findById(id).orElse(null);
     }
 
     @Override
     public Boolean deleteJobById(Long id) {
-        Iterator<Job> iterator = jobs.iterator();
-        while(iterator.hasNext()){
-            Job job = iterator.next();
-            if(job.getId().equals(id)){
-                iterator.remove();
-                return true;
-            }
+        try {
+            jobRepository.deleteById(id);
+            return true;
+        }catch (Exception e){
+            return false;
+
         }
-        return false;
+
     }
 
     @Override
     public boolean updateJob(Long id, Job updatedJob) {
-        Iterator<Job> iterator = jobs.iterator();
-        while(iterator.hasNext()){
-            Job job = iterator.next();
-            if(job.getId().equals(id)){
-                job.setTitle(updatedJob.getTitle());
-                job.setDescription(updatedJob.getDescription());
-                job.setLocation(updatedJob.getLocation());
-                job.setMaxSalary(updatedJob.getMaxSalary());
-                job.setMinSalary(updatedJob.getMinSalary());
-                return true;
-            }
+        Optional<Job> jobOptional = jobRepository.findById(id);
+        if (jobOptional.isPresent()) {
+            Job job = jobOptional.get();
+            job.setTitle(updatedJob.getTitle());
+            job.setDescription(updatedJob.getDescription());
+            job.setLocation(updatedJob.getLocation());
+            job.setMaxSalary(updatedJob.getMaxSalary());
+            job.setMinSalary(updatedJob.getMinSalary());
+            jobRepository.save(job);
+            return true;
         }
         return false;
     }
